f6d9913dde608012044c64f59ca02529d0552348
H-DEV-F <freirehp@gmail.com>
2020-09-13 00:04:34 -0300

    Scaffolding creation

---
+++ WebAppCondominio/Data/WebAppCondominioContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppCondominio.Models;
+
+namespace WebAppCondominio.Data
+{
+    public class WebAppCondominioContext : DbContext
+    {
+        public WebAppCondominioContext (DbContextOptions<WebAppCondominioContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppCondominio.Models.Condominio> Condominio { get; set; }
+    }
+}
