0dfb8ba39a12a1167135cf5a6d9c09e0e5946a18
henrique pinheiro <henrique.p.freire@hotmail.com>
2025-07-06 20:05:03 -0300

    feat: alter logger config

--- AdmCondominio.Api/Config/LoggerConfig.cs
+++ AdmCondominio.Api/Config/LoggerConfig.cs
@@ -6,20 +6,7 @@ namespace AdmCondominio.Api.Config
     {
         public static IServiceCollection AddLoggingConfig(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddElmahIo(o =>
-            {
-                o.ApiKey = "b76edc95d42c414fab7e46ebcef840a0";
-                o.LogId = new Guid("b7205ec8-b5af-45e7-99a5-531570a75232");
-            });
-
             services.AddHealthChecks()
-                .AddElmahIoPublisher(options =>
-                {
-                    options.ApiKey = "b76edc95d42c414fab7e46ebcef840a0";
-                    options.LogId = new Guid("b7205ec8-b5af-45e7-99a5-531570a75232");
-                    options.HeartbeatId = "171c973ba94b4c2fb02466e2097f9906";
-
-                })
                 .AddCheck("Db_Query", new SqlServerHealthCheck(configuration.GetConnectionString("DefaultConnection")))
                 .AddSqlServer(configuration.GetConnectionString("DefaultConnection"), name: "Db_Connection");
 
@@ -31,11 +18,6 @@ namespace AdmCondominio.Api.Config
             return services;
         }
 
-        public static IApplicationBuilder UseLoggingConfiguration(this IApplicationBuilder app)
-        {
-            app.UseElmahIo();
-
-            return app;
-        }
+        public static IApplicationBuilder UseLoggingConfiguration(this IApplicationBuilder app) => app;
     }
 }
